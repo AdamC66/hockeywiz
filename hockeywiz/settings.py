baf3beed00c805af90587dbd77057c4a7b8a09e4
adamc66 <adam.cote66@gmail.com>
2019-12-05 16:49:14 -0500

    changed favicon and title finally

--- hockeywiz/settings.py
+++ hockeywiz/settings.py
@@ -103,7 +103,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hockeywiz',
+        'NAME': 'hockeywiztest',
     }
 }
 
